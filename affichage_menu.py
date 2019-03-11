948a1430cfca9cfbea589700e6c9652b441adf50
Irene <irene.pawlowski91@gmail.com>
2019-03-11 16:31:01 +0100

    Ajout de la fonction nouvelle note sur la portee dans le menu portee

--- affichage_menu.py
+++ affichage_menu.py
@@ -5,10 +5,33 @@ from PIL import Image
 from PIL import ImageTk
 from tkinter import messagebox
 
+def affiche_note():
+
+    note = tk.PhotoImage(file="note_taille.png")
+    canvas.pack(expand=YES, fill=BOTH)
+    def _create_circle(self, x, y, r, **kwargs):
+        return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+    tk.Canvas.create_circle = _create_circle
+    canvas.create_circle(200, 200, 25, fill="black")
+    canvas.create_image(10,10,image=note, anchor=NW)
+    """
+    w2=tk.Label(root, compound = tk.LEFT, image=note).pack(side="left")
+    w2.pack()
+    """
+
 def affiche_portee():
+    cle_de_sol = tk.PhotoImage(file="cle_de_sol.png")    
+    canvas.pack(expand=YES, fill =BOTH)
+    for i in range(6):
+        canvas.create_line(0,50*i,400,50*i)
+    canvas.create_image(0,0, image=cle_de_sol, anchor=NW)
+    """ 
+    BOUT DE CODE FONCTIONNANT MAIS PAS EN ARRIERE PLAN
+
     portee = tk.PhotoImage(file="portee.png")
     w1=tk.Label(root, compound = tk.CENTER, image=portee).pack(side="right")
     w1.pack()
+    """
 
 def donothing():
    filewin = Toplevel(root)
@@ -16,7 +39,7 @@ def donothing():
    button.pack()
 
 root = tk.Tk()
-
+canvas = Canvas(width=500, height = 500, bg='white')
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="New", command=donothing)
@@ -32,24 +55,28 @@ menubar.add_cascade(label="File", menu=filemenu)
 
 porteemenu=Menu(menubar, tearoff=0)
 porteemenu.add_command(label="Nouvelle portee", command=affiche_portee)
+porteemenu.add_command(label="Ajouter note", command=affiche_note)
 menubar.add_cascade(label="Portee", menu=porteemenu)
+
 """
 editmenu = Menu(menubar, tearoff=0)
 editmenu.add_command(label="Undo", command=donothing)
 
 editmenu.add_separator()
 
-editmenu.add_command(label="Cut", command=donothing)
+editmenu.add_command(label="Ajouter Note", command=affiche_note)
+menubar.add_cascade(label="Edit", menu=editmenu)
+
 editmenu.add_command(label="Copy", command=donothing)
 editmenu.add_command(label="Paste", command=donothing)
 editmenu.add_command(label="Delete", command=donothing)
 editmenu.add_command(label="Select All", command=donothing)
 
-menubar.add_cascade(label="Edit", menu=editmenu)
+
 helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label="Help Index", command=donothing)
 helpmenu.add_command(label="About...", command=donothing)
 menubar.add_cascade(label="Help", menu=helpmenu)
 """
 root.config(menu=menubar)
-root.mainloop()
+mainloop()
