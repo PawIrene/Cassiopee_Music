45d35366b0865ea589de03ae4e99dc6cb80f1085
Jeanpopes <36034450+Jeanpopes@users.noreply.github.com>
2019-03-11 19:48:24 +0100

    mise a jour nouvelle gestion wav
    on modifie pour permettre la compatibilite avec la nouvelle gestion plus simple et rapide de wav

--- shepard.py
+++ shepard.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Ce module permet de realiser des tableaux d'entier directement exploitable par le module wav.py pour manipuler les sons de Shepard.
+Ce module permet de realiser des tableaux d'entier directement exploitable par 
+le module wav.py pour manipuler les sons de Shepard.
 """
 import wav as w
-import math as m
+import numpy as np
 
 def note(fNote, fech=44100, duree=3, volume=0.5, nbOctet=2):
     """
@@ -11,11 +12,15 @@ def note(fNote, fech=44100, duree=3, volume=0.5, nbOctet=2):
     de frequence fNote echantillonee a fech en Hz quantifie sur nbOctet sur une
     duree de duree s.
     
-    fNote float positif en Hz
-    fech entiers positifs en Hz
-    duree en s positive
-    volume float entre 0 et 1
-    nbOctet un entier entre 1 et 2
+    Le premier element du tableau est un tuple contenant tout les parametres
+    du fichier sonore sous la forme :
+    (nchannels, sampwidth, framerate, nframes, "NONE", "not compressed" )
+    
+    - fNote float positif en Hz
+    - fech entiers positifs en Hz
+    - duree en s positive
+    - volume float entre 0 et 1
+    - nbOctet un entier entre 1 et 2
     """
     
     assert (type(fech) == int and fech > 0),"la frequence d echantillonage\
@@ -27,65 +32,78 @@ def note(fNote, fech=44100, duree=3, volume=0.5, nbOctet=2):
 "le nombre d'octet par echantillon doit valoir 1 ou 2"
     assert (0<=volume<=1),"volume doit etre compris entre 0 et 1"
     
-    
+
     
     data = [0]*(int(duree*fech))
     if(nbOctet == 1):
         for i in range(len(data)):
-            data[i] = (int(128 + 127*volume*m.sin(2*m.pi*fNote*i/fech)))
+            data[i] = (int(128 + 127*volume*np.sin(2*np.pi*fNote*i/fech)))
     else: #nbOctet == 2
         for i in range(len(data)):
-            data[i] = (int(32767*volume*m.sin(2*m.pi*fNote*i/fech)))
-    return data
+            data[i] = (int(32767*volume*np.sin(2*np.pi*fNote*i/fech)))
+    params = [w.new_param(len(data),nbOctet,fech)]
+    return params + data
     
     
     
-def noteShepard(fFond, duree=1, volume=0.5, nbOctet=2):
+def noteShepard(fFond, duree=1, volume=0.5, nbOctet=2, fech = 44100):
     """
     retourne le tableau d entier correspondant a la note de Shepard
-    de frequence fondamentale fNote en HZ echantillonee a 44100Hz quantifie sur 
+    de frequence fondamentale fNote en HZ echantillonee a fech quantifie sur 
     nbOctet sur une duree de duree s.
     
-    fNote et fech en Hz des entiers positifs
-    duree en s positive
-    volume float entre 0 et 1
-    nbOctet un entier entre 1 et 2
+    Le premier element du tableau est un tuple contenant tout les parametres
+    du fichier sonore sous la forme :
+    (nchannels, sampwidth, framerate, nframes, "NONE", "not compressed" )
+    
+    - fFond et fech en Hz des entiers positifs
+    - duree en s positive
+    - volume float entre 0 et 1
+    - nbOctet un entier entre 1 et 2
     """
     
     fech = 44100
-    listeHarmo = [[]]*10 #la liste des différetes harmoniques audibles
+    listeHarmo = [[]]*10 #la liste des differentes harmoniques audibles
     for i in range(len(listeHarmo)):
-        listeHarmo[i] = note(fFond*(2**i),fech,duree,volume,nbOctet)
+        listeHarmo[i] = note(fFond*(2**i),fech,duree,volume,nbOctet)[1:]
     data = [0]*(int(duree*fech))
     for i in range(len(data)):
         somme = 0
         for j in range(len(listeHarmo)):
             somme += listeHarmo[j][i]
         data[i] = somme//10
-    return data
+    params = [w.new_param(len(data),nbOctet,fech)]
+    return params + data
 
 
    
 
-def gammeShepard(nbRepet=1,dureeNote=1,dureeSilence=1,volume=0.5,nbOctet=2\
-,listFond=[16.35*2**(i/12) for i in range(12)]):
+def gammeShepard(nbRepet=1,dureeNote=0.35,dureeSilence=0.15,volume=0.5,nbOctet=2\
+,listFond=[16.35*2**(i/12) for i in range(12)],fech = 44100):
     """
     retourne le tableau d entier correspondant a la gamme de Shepard
-    echantillonee a 44100Hz quantifie sur nbOctet repetee nbRepet fois
+    echantillonee a fech quantifie sur nbOctet repetee nbRepet fois
     avec des duree de note de dureeNote secondes et des silences de
     dureeSilence secondes.
     
-    nbRepet entier positif
-    duree en s positive
-    dureeSilence en s positive
-    volume float entre 0 et 1
-    nbOctet un entier entre 1 et 2
+    Le premier element du tableau est un tuple contenant tout les parametres
+    du fichier sonore sous la forme :
+    (nchannels, sampwidth, framerate, nframes, "NONE", "not compressed" )
+    
+    - nbRepet entier positif
+    - duree en s positive
+    - dureeSilence en s positive
+    - volume float entre 0 et 1
+    - nbOctet un entier entre 1 et 2
+    - fech en Hz entier positif
+    - listFond un tableau contenant dans l'ordre chronologique la liste
+    des valeurs en Herz des fondamentales des notes de chepard desirees
     """
     
     data = []
-    silence = note(0, 44100, dureeSilence, 0, nbOctet)
+    silence = [0]*int(fech*dureeSilence)
     for i in range(nbRepet):
         for j in range(len(listFond)):
-            data += noteShepard(listFond[j],dureeNote,volume,nbOctet) + silence
-
-    return data 
+            data += noteShepard(listFond[j],dureeNote,volume,nbOctet,fech)[1:] + silence
+    params = [w.new_param(len(data),nbOctet,fech)]
+    return params + data 
