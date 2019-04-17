b9bf3d04a64d2f71cbca09f08d2cfc80fb654a9a
Irene <irene.pawlowski91@gmail.com>
2019-04-17 11:46:32 +0200

    Modification de l'affichage pour la démo : Lien avec shépard et inversion de spectre

--- demo_affichage.py
+++ demo_affichage.py
@@ -8,27 +8,42 @@ import son
 import shepard
 import matplotlib.pyplot as plt
 
-def donothing():
-   filewin = Toplevel(root)
-   button = Button(filewin, text="Do nothing button")
-   button.pack()
+def create_shepard():
+   donnee = shepard.noteShepard(13)
+   donnee.graphFFT(fmax = 7500)
+   plt.show()
+   donnee.ecrire("noteShepard")
 
-def create_wav(e1,e2,e3):
-   """
+def inversion_spectre():
+   donnee = son.ouvrir("la")
+   donnee.graphFFT()
+   donnee.inverseFFT()
+   donnee.graphFFT()
+   plt.show()
+   donnee.decalFFT(-19000, [19100,22050])
+   donnee.graphFFT()
+   plt.show()
+   donnee.ecrire("cassiopeReverse")
    
-   PROBLEMES DE CONVERSION DE STRING VERS FLOAT
+def create_wav(e1,e2,e3):
+   
+   
+   donnee = shepard.note(440)
+   donnee.graphFFT(fmax=550)
+   plt.show()
+   donnee.ecrire("la")
+   
+   """
+ 
 
-   freq=float(e2.get())
-   fmax=float(e3.get())
+   #freq=float(e2.get())
+   #fmax=float(e3.get())
    donnee=shepard.note(float(e2.get()))
    donnee.graphFFT(fmax=e3.get())
    plt.show()
    donnee.ecrire(e1.get()) 
    """
-   donnee = shepard.note(440)
-   donnee.graphFFT(fmax=550)
-   plt.show()
-   donnee.ecrire("la")
+   
    
 def graph_enter_creation():
    filewin = Toplevel(root)
@@ -44,9 +59,12 @@ def graph_enter_creation():
    e2.grid(row=1, column=1)
    e3.grid(row = 2, column=1)
    
-   Button(filewin, text='Enter', command= create_wav(e1,e2,e3) ).grid(row=3, column=1, sticky=W, pady=4)
+   #Button(filewin, text='Créer le son', command= create_wav(e1,e2,e3) ).grid(row=3, column=1, sticky=W, pady=4)
 
    Button(filewin, text='Quit', command=filewin.quit).grid(row=3, column=0, sticky=W, pady=4)
+
+   Button(filewin, text = "Créer gamme de Shépard", command = create_shepard).grid(row=3, column=2, sticky=W, pady=4)
+   Button(filewin, text = "inverser le spectre", command = inversion_spectre).grid(row=3, column=3, sticky=W, pady=4)
    
 def open_data(e1):
     for element in os.listdir('.'):
@@ -61,9 +79,11 @@ def open_son():
    Label(filewin, text="Name").grid(row=0)
    e1 = Entry(filewin)
    e1.grid(row=0, column=1)
-   Button(filewin, text='Quit', command=filewin.quit).grid(row=3, column=0, sticky=W, pady=4)
-   opendata=open_data(e1)
-   Button(filewin, text='Enter', command= opendata ).grid(row=3, column=1, sticky=W, pady=4)
+   
+   Button(filewin, text='Quitter', command=filewin.quit).grid(row=3, column=0, sticky=W, pady=4)
+   Button(filewin, text='Ouvrir le fichier', command= open_data(e1) ).grid(row=3, column=1, sticky=W, pady=4)
+   Button(filewin, text = "Créer gamme de Shépard", command = create_shepard).grid(row=3, column=2, sticky=W, pady=4)
+   Button(filewin, text = "inverser le spectre", command = inversion_spectre).grid(row=3, column=3, sticky=W, pady=4)
 
    
 root = tk.Tk()
@@ -77,15 +97,10 @@ filemenu.add_cascade(label="New", menu= newmenu)
 
 filemenu.add_command(label="Open", command=open_son)
 
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_command(label="Save as...", command=donothing)
-filemenu.add_command(label="Close", command=donothing)
-
 filemenu.add_separator()
 
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
-
 
 root.config(menu=menubar)
 mainloop()
