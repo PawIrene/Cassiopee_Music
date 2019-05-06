44ce19280bd744f930b14dd0bc4a29359a8372d5
Irene <irene.pawlowski91@gmail.com>
2019-05-06 16:58:18 +0200

    Modification de l'affichage avec tkinter sur demo_affichage.py

--- demo_affichage.py
+++ demo_affichage.py
@@ -24,101 +24,86 @@ def inversion_spectre():
    donnee.graphFFT()
    plt.show()
    donnee.ecrire("cassiopeReverse")
-   
-def create_wav(e1,e2,e3):
-   
-   
-   donnee = shepard.note(440)
-   donnee.graphFFT(fmax=550)
-   plt.show()
-   donnee.ecrire("la")
-   
-   """
- 
 
-   #freq=float(e2.get())
-   #fmax=float(e3.get())
-   donnee=shepard.note(float(e2.get()))
-   donnee.graphFFT(fmax=e3.get())
-   plt.show()
-   donnee.ecrire(e1.get()) 
-   """
-   
-   
-def graph_enter_creation():
+def create_son(e1):
+   donnee = shepard.note(int(e1))   
+   print("Création de la note de fréquence fondamentale", int(e1))
+
+def create_son_shepard(e1):
+   donnee = shepard.noteShepard(int(e1))
+   print("Creation de la note de Shepard de fréquence fondamentale", int(e1))
+
+def create_note_shepard():
    filewin = Toplevel(root)
-   Label(filewin, text="Nom").grid(row=0)
-   Label(filewin, text="Fréquence de la note").grid(row=1)
-   Label(filewin, text="Fréquence max").grid(row=2)
-   
+   TEXTE = "Le nombre d'octet est fixé à 2. La durée est fixée à 3 secondes et le volume est à 0.5."
+   Label(filewin, text = TEXTE).grid(row=0)
+   Label(filewin, text="Fréquence fondamentale de la note (Hz) : ").grid(row=1)
    e1 = Entry(filewin)
-   e2 = Entry(filewin)
-   e3 = Entry(filewin)
+   e1.grid(row=1, column=1)  
+   Button(filewin, text='Créer la note de Shepard', command= lambda : create_son_shepard(e1.get())).grid(row=3, column=1, sticky=W, pady=4)
+   Button(filewin, text='Quit', command=filewin.quit).grid(row=3, column=0, sticky=W, pady=4)   
    
-   e1.grid(row=0, column=1)
-   e2.grid(row=1, column=1)
-   e3.grid(row = 2, column=1)
-   
-   #Button(filewin, text='Créer le son', command= create_wav(e1,e2,e3) ).grid(row=3, column=1, sticky=W, pady=4)
-
+def create_note():
+   filewin = Toplevel(root)
+   TEXTE = "Le nombre d'octet est fixé à 2 et la fréquence d'échantillonnage est déjà fixée à 44 100 Hz. La durée est fixée à 3 secondes et le volume est à 0.5."
+   Label(filewin, text = TEXTE).grid(row=0)
+   Label(filewin, text="Fréquence fondamentale de la note (Hz) : ").grid(row=1)
+   e1 = Entry(filewin)
+   e1.grid(row=1, column=1)  
+   Button(filewin, text='Créer le son', command= lambda : create_son(e1.get())).grid(row=3, column=1, sticky=W, pady=4)
    Button(filewin, text='Quit', command=filewin.quit).grid(row=3, column=0, sticky=W, pady=4)
-
-   Button(filewin, text = "Créer gamme de Shépard", command = create_shepard).grid(row=3, column=2, sticky=W, pady=4)
-   Button(filewin, text = "inverser le spectre", command = inversion_spectre).grid(row=3, column=3, sticky=W, pady=4)
    
 def open_data(e1):
+   var = son.ouvrir(e1)
+   print(var.fech)
+   """
     for element in os.listdir('.'):
        if element == e1.get():
           print("Le fichier %s existe \n" % (e1.get()))
           print("hEllO WOrlD")
        else:
           print("**\n")
-   
+   """
 def open_son():
    filewin = Toplevel(root)
-   Label(filewin, text="Name").grid(row=0)
+   Label(filewin, text="Nom du fichier .wav").grid(row=0)
    e1 = Entry(filewin)
    e1.grid(row=0, column=1)
    
    Button(filewin, text='Quitter', command=filewin.quit).grid(row=3, column=0, sticky=W, pady=4)
-   Button(filewin, text='Ouvrir le fichier', command= open_data(e1) ).grid(row=3, column=1, sticky=W, pady=4)
-   Button(filewin, text = "Créer gamme de Shépard", command = create_shepard).grid(row=3, column=2, sticky=W, pady=4)
-   Button(filewin, text = "inverser le spectre", command = inversion_spectre).grid(row=3, column=3, sticky=W, pady=4)
-
+   Button(filewin, text='Ouvrir le fichier', command= open_data(e1.get()) ).grid(row=3, column=1, sticky=W, pady=4)
    
 root = tk.Tk()
 
 ecran_accueil = Canvas(root, width=500, height=500)
-#ecran_accueil.create_image(250, 250, image = 'Bienvenue.png')
 
 photo = PhotoImage(file='Bienvenue.png')
 item = ecran_accueil.create_image(250,250, image=photo)
 ecran_accueil.pack()
 
-#TEXTE = "Bienvenue sur l'application d'étude de signaux musicaux et acoustiques."
-TEXTE2 = "Pour créer un fichier .wav, veuillez cliquer sur File puis New."
-TEXTE3 = "Pour ouvrir un fichier .wav déjà existant, veuillez cliquer sur File puis Open."
+TEXTE2 = "Pour créer un fichier son, veuillez cliquer sur Fichier puis Nouveau."
+TEXTE3 = "Pour ouvrir un fichier son déjà existant, veuillez cliquer sur Fichier puis Ouvrir."
 
-#label = tk.Label(root, text = TEXTE, wraplength = 500, justify = tk.CENTER)
-#label.pack()
 label2 = tk.Label(root, text = TEXTE2, wraplength = 500, justify = tk.CENTER)
 label2.pack()
 label3 = tk.Label(root, text = TEXTE3, wraplength = 500, justify = tk.CENTER)
 label3.pack()
+
 menubar= Menu(root)
 
 filemenu = Menu(menubar, tearoff = 0)
 
 newmenu = Menu(menubar, tearoff = 0)
-newmenu.add_command(label="New wav", command=graph_enter_creation)
-filemenu.add_cascade(label="New", menu= newmenu)
+newmenu.add_command(label="Note", command=create_note)
+newmenu.add_command(label="Note de Shepard", command = create_note_shepard)
+filemenu.add_cascade(label="Nouveau", menu= newmenu)
 
-filemenu.add_command(label="Open", command=open_son)
+filemenu.add_command(label="Ouvrir", command=open_son)
 
 filemenu.add_separator()
 
 filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
+menubar.add_cascade(label="Fichier", menu=filemenu)
 
 root.config(menu=menubar)
 mainloop()
